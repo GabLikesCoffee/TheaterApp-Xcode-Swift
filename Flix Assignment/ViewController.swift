4f304a7617ba10e166497d69329473b9a79c653c
GabLikesCoffee <gabisgr8@gmail.com>
2022-09-07 16:21:18 -0400

    Finished adding posters

--- Flix Assignment/ViewController.swift
+++ Flix Assignment/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -57,6 +58,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         cell.titleLabel.text = title as? String
         cell.synLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af.setImage(withURL: posterUrl!)
+        
         return cell
     }
     
